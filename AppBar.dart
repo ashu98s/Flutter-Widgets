caae422fe16db65b3e0c7f7c7ab3b536a1366f97
ashu98s <ashirbad.dkl24@gmail.com>
2022-01-11 10:35:44 +0530

    Flutter: AppBar

---
+++ AppBar.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+    runApp(
+        MaterialApp(
+            home: Scaffold(
+                appBar: AppBar(
+                    title: Text("Flutter Codes"),
+                    backgroudColor: Colors.blueGrey[900],
+                ), // AppBAr
+            ), // Scaffold
+        ), // MaterialApp
+    );
+}
